f48c3dc7123352e77c6253b82fe7b1dd3b481020
sudogitpush <232553754+sudogitpush@users.noreply.github.com>
2025-11-26 17:23:58 +0100

    multistage - user added

---
+++ linux_cli/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Running tests"
+echo "Success"
+exit 0
+
+# Domnivam se, ze v kurzu jsme se psat testy na bash script neucili. 
+# Dle meho nazoru ukol slouzi k procviceni probrane latky, coz testovani sh scriptu neni.
+# Z tohoto duvodu prikladam pouze dummy test.
+
